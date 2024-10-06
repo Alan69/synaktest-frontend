dc74ff0cb9f59c0a11efd48fb7593338533ab76c
dastanSharpDev <dastan.kudaibergen@sharp-dev.net>
2024-10-06 18:38:46 +0500

    hide footer and header when test is started

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,19 +1,23 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import styles from './MainLayout.module.scss'
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+import { useTypedSelector } from 'hooks/useTypedSelector';
 
 const MainLayout = () => {
   const location = useLocation();
+  const { id } = useParams();
+
+  const { user } = useTypedSelector((state) => state.auth);
 
   return (
     <div className='page-wrapper relative z-[1] bg-white'>
-      <Header />
+      {user?.test_is_started && location.pathname.includes(`${'product/'}${id}`) ? '' : <Header />}
       <main className={styles.mainLayout}>
         <Outlet />
       </main>
       {/* {location.pathname.includes('product') ? '' : <Footer />} */}
-      <Footer />
+      {user?.test_is_started && location.pathname.includes(`${'product/'}${id}`) ? '' : <Footer />}
     </div>
   );
 };
