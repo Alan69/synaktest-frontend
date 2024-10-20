cf572eeef2c6c5342461eaee193551c636409e5f
dastanSharpDev <dastan.kudaibergen@sharp-dev.net>
2024-10-20 17:23:49 +0500

    fix bug

--- src/modules/competed-test/components/ModalWorkOnErrors/ModalWorkOnErrors.tsx
+++ src/modules/competed-test/components/ModalWorkOnErrors/ModalWorkOnErrors.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import { Button, Modal, Typography } from 'antd';
-import {
-  CheckCircleOutlined,
-  CloseCircleOutlined
-} from '@ant-design/icons';
-import cn from 'classnames';
-import styles from './ModalWorkOnErrors.module.scss';
-import { TCompletedQuestion } from 'modules/competed-test/redux/api';
+import React from "react";
+import { Button, Modal, Typography } from "antd";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import cn from "classnames";
+import styles from "./ModalWorkOnErrors.module.scss";
+import { TCompletedQuestion } from "modules/competed-test/redux/api";
 
 type TProps = {
-  isOpen: boolean
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
-  selectedQuestion: TCompletedQuestion | null
-  subjectTitle: string
-}
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedQuestion: TCompletedQuestion | null;
+  subjectTitle: string;
+};
 
 export const ModalWorkOnErrors = ({
   isOpen,
   setIsOpen,
   selectedQuestion,
-  subjectTitle
+  subjectTitle,
 }: TProps) => {
   const handleCancel = () => {
     setIsOpen(false);
@@ -46,36 +43,51 @@ export const ModalWorkOnErrors = ({
             Вопрос - {selectedQuestion.question_text}
           </Typography.Title>
 
-          {
-            selectedQuestion.selected_option === null ?
-              <Typography.Title level={5}>
-                Вы не выбрали вариант ответа!
-              </Typography.Title> :
-              ''
-          }
+          {selectedQuestion.selected_option === null ? (
+            <Typography.Title level={5}>
+              Вы не выбрали вариант ответа!
+            </Typography.Title>
+          ) : (
+            ""
+          )}
 
           <div className={styles.options}>
-            {selectedQuestion.all_options.map((option) => {
-              const isSelected = selectedQuestion.selected_option?.id === option.id;
-              const isCorrect = option.is_correct;
+            {selectedQuestion?.all_options?.length > 0 ? (
+              selectedQuestion.all_options.map((option) => {
+                const isSelected =
+                  selectedQuestion.selected_option?.id === option.id;
+                const isCorrect = option.is_correct;
 
-              return (
-                <div
-                  key={option.id}
-                  className={cn(
-                    styles.option,
-                    isSelected ? styles.selectedOption : '',
-                    isCorrect ? styles.option__correct : styles.option__inCorrect,
-                    isSelected && isCorrect ? styles.option__correctNSelected : ''
-                  )}
-                >
-                  <span className={styles.optionLabel}>
-                    {isCorrect ? <CheckCircleOutlined /> : <CloseCircleOutlined />}
-                  </span>
-                  <span>{option.text}</span>
-                </div>
-              );
-            })}
+                return (
+                  <div
+                    key={option.id}
+                    className={cn(
+                      styles.option,
+                      isSelected ? styles.selectedOption : "",
+                      isCorrect
+                        ? styles.option__correct
+                        : styles.option__inCorrect,
+                      isSelected && isCorrect
+                        ? styles.option__correctNSelected
+                        : ""
+                    )}
+                  >
+                    <span className={styles.optionLabel}>
+                      {isCorrect ? (
+                        <CheckCircleOutlined />
+                      ) : (
+                        <CloseCircleOutlined />
+                      )}
+                    </span>
+                    <span>{option.text}</span>
+                  </div>
+                );
+              })
+            ) : (
+              <Typography.Title level={5}>
+                Нет доступных вариантов ответа.
+              </Typography.Title>
+            )}
           </div>
         </div>
       )}
