7034197a72c41858c4915271ae0b56f64119df66
Alan69 <gameg1676@gmail.com>
2025-03-24 01:02:48 +0500

    fix

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -6,7 +6,7 @@ import { RootState } from 'redux/rootReducer';
 const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://127.0.0.1:8000/api/',
+    baseUrl: 'https://api.synaqtest.kz/api/',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
 
@@ -21,7 +21,7 @@ const baseApi = createApi({
   // @ts-ignore
   async baseQuery(args, api, extraOptions) {
     let result = await fetchBaseQuery({
-      baseUrl: 'http://127.0.0.1:8000/api/',
+      baseUrl: 'https://api.synaqtest.kz/api/',
       prepareHeaders: (headers, { getState }) => {
         const token = (getState() as RootState).auth.token;
         if (token) {
@@ -36,7 +36,7 @@ const baseApi = createApi({
 
       if (refreshToken) {
         const refreshResult = await fetchBaseQuery({
-          baseUrl: 'http://127.0.0.1:8000/api/',
+          baseUrl: 'https://api.synaqtest.kz/api/',
         })(
           {
             url: '/token/refresh/',
@@ -52,7 +52,7 @@ const baseApi = createApi({
           api.dispatch(authActions.setToken({ token: newTokens.access, refreshToken: newTokens.refresh }));
 
           result = await fetchBaseQuery({
-            baseUrl: 'http://127.0.0.1:8000/api/',
+            baseUrl: 'https://api.synaqtest.kz/api/',
             prepareHeaders: (headers) => {
               headers.set('Authorization', `Bearer ${newTokens.access}`);
               return headers;
